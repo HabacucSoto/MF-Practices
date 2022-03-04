e7a0d450dc52f62a0aab447e877eb538d81c8954
habacuc <josehabacuc182020@gmail.com>
2022-03-04 14:54:56 -0600

    last details

--- 02-HTML/server.js
+++ 02-HTML/server.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = process.env.port || 3000
+var express = require('express');
+var path = require('path');
 
-const adminRouter = express.Router();
+// Crea una Express app.
+var app = express();
 
-//página principal del admin, panel de administración/dashboard (http://localhost:1337/admin)
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+// Puerto
+const port = process.env.port || 8080
+
+// obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
+var publicPath = path.resolve(__dirname, 'public'); //path.join(__dirname, 'public'); también puede ser una opción
+
+// Para que los archivos estaticos queden disponibles.
+app.use(express.static(publicPath));
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/public/index.html');
 });
 
-app.get('/personalizado', function (req, res) {
+app.get('/personalizado', function(req, res){
     res.sendFile(__dirname + '/public/personalizado.html');
 });
 
-app.get('/administrador', function(req, res) { 
+app.get('/administrador', function(req, res){ 
     res.sendFile(__dirname + '/public/admin.html');
 });
 
-app.listen(port, () => {
-    console.log('server: ' + port)
-})
+app.listen(port)
